d2dc009ef426e99bb666316975fd57cdb8fb7d91
clink25 <168674556+clink25@users.noreply.github.com>
2024-05-03 15:21:00 -0500

    Update shared_code_combined_fasta.py

--- shared_code_combined_fasta.py
+++ shared_code_combined_fasta.py
@@ -1,3 +1,18 @@
 # code for the phylogenetic tree
+from Bio import SeqIO
 
+def combine_fasta_files(*fasta_files):
+    combined_sequences = []
+
+    for fasta_file in fasta_files:
+        with open(fasta_file, "r") as file:
+            sequences = list(SeqIO.parse(file, "fasta"))
+            combined_sequences.extend(sequences)
+            
+    combined_file = "combined_organisms.fasta"  # Define the output file name here
+    
+    with open(combined_file, "w") as output_file:
+        SeqIO.write(combined_sequences, output_file, "fasta")
+        
+    return output_file
 
